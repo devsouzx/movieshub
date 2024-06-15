75346e494a04d8ad4aeaa577eaba828424e7d68e
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-15 14:53:58 -0300

    Find user by id & user not found exception

--- src/main/java/com/devsouzx/movieshub/resources/UserResource.java
+++ src/main/java/com/devsouzx/movieshub/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.devsouzx.movieshub.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,11 @@ public class UserResource {
     public ResponseEntity<List<User>> findUsers() {
         List<User> users = userService.findAll();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUser(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/devsouzx/movieshub/services/UserService.java
+++ src/main/java/com/devsouzx/movieshub/services/UserService.java
@@ -2,10 +2,13 @@ package com.devsouzx.movieshub.services;
 
 import com.devsouzx.movieshub.domain.User;
 import com.devsouzx.movieshub.repositories.UserRepository;
+import com.devsouzx.movieshub.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.objenesis.ObjenesisException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("User not Found"));
     }
 }
