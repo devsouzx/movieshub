0864ef986c03f6ddc1c9826fc4b2d24cc1807b54
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-15 14:05:53 -0300

    first commit

---
+++ src/main/java/com/devsouzx/movieshub/MovieshubApplication.java
@@ -0,0 +1,11 @@
+package com.devsouzx.movieshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieshubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MovieshubApplication.class, args);
+	}
+}
