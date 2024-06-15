f586ebae78e3017ddeb019fd7aa13e012cce2a7f
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-15 18:30:54 -0300

    Find movie by id

--- src/main/java/com/devsouzx/movieshub/resources/MovieResource.java
+++ src/main/java/com/devsouzx/movieshub/resources/MovieResource.java
@@ -1,10 +1,12 @@
 package com.devsouzx.movieshub.resources;
 
 import com.devsouzx.movieshub.domain.Movie;
+import com.devsouzx.movieshub.domain.User;
 import com.devsouzx.movieshub.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +18,15 @@ public class MovieResource {
     @Autowired
     private MovieService movieService;
 
-
     @GetMapping
     public ResponseEntity<List<Movie>> findAll() {
         List<Movie> movies = movieService.findAll();
         return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> findUser(@PathVariable String id) {
+        Movie movie = movieService.findById(id);
+        return ResponseEntity.ok(movie);
     }
 }
--- src/main/java/com/devsouzx/movieshub/services/MovieService.java
+++ src/main/java/com/devsouzx/movieshub/services/MovieService.java
@@ -1,11 +1,14 @@
 package com.devsouzx.movieshub.services;
 
 import com.devsouzx.movieshub.domain.Movie;
+import com.devsouzx.movieshub.domain.User;
 import com.devsouzx.movieshub.repositories.MovieRepository;
+import com.devsouzx.movieshub.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +17,10 @@ public class MovieService {
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    public Movie findById(String id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.orElseThrow(() -> new ObjectNotFoundException("Movie not Found"));
     }
 }
